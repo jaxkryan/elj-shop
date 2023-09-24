db635c0b6e6f936f632fa0e3bbd9c20778a14405
Huyldhe <huyg.a22052003@gmail.com>
2023-09-24 20:46:08 +0700

    asc desc, view product and edit qty of product

---
+++ src/java/controller/StorageController.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.ProductDAO;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Product;
+
+/**
+ *
+ * @author Datalia
+ */
+@WebServlet(name = "SortController", urlPatterns = {"/SortController"})
+public class StorageController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            ProductDAO productDAO = new ProductDAO();
+            String service = request.getParameter("go");
+            String submit = request.getParameter("submit");
+            String search = request.getParameter("search");
+            if (service == null || service.equals("All")) {
+                if (submit != null) {
+                    int pid = Integer.parseInt(request.getParameter("prodId"));
+                    int quantity = Integer.parseInt(request.getParameter("qty"));
+                    ProductDAO dao = new ProductDAO();
+                    try {
+                        dao.updateQty(pid, quantity);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(StorageController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                RequestDispatcher disp = request.getRequestDispatcher("/jsp/manageStoragePage.jsp");
+                Vector<Product> products = productDAO.getAllProduct();
+                request.setAttribute("products", products);
+                disp.forward(request, response);
+                service = "All";
+            }
+            if (service.equals("Asc")) {
+                if (submit != null) {
+                    int pid = Integer.parseInt(request.getParameter("prodId"));
+                    int quantity = Integer.parseInt(request.getParameter("qty"));
+                    ProductDAO dao = new ProductDAO();
+                    try {
+                        dao.updateQty(pid, quantity);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(StorageController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                RequestDispatcher disp = request.getRequestDispatcher("/jsp/ascStoragePage.jsp");
+                Vector<Product> products = productDAO.sortProducts("quantity", "Asc");
+                request.setAttribute("products", products);
+                disp.forward(request, response);
+                service = "Asc";
+            }
+            if (service.equals("Desc")) {
+                if (submit != null) {
+                    int pid = Integer.parseInt(request.getParameter("prodId"));
+                    int quantity = Integer.parseInt(request.getParameter("qty"));
+                    ProductDAO dao = new ProductDAO();
+                    try {
+                        dao.updateQty(pid, quantity);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(StorageController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (search!=null) {
+                    String keyword = request.getParameter("keyword");
+                    String statement = "Select * from product where name like '%" + keyword + "%' ";
+                    Vector<Product> products = productDAO.searchProducts(statement, "order by quantity desc");
+                    request.setAttribute("products", products);
+                    request.getRequestDispatcher("/jsp/descStoragePage.jsp").forward(request, response);
+                }
+                else{
+                RequestDispatcher disp = request.getRequestDispatcher("/jsp/descStoragePage.jsp");
+                Vector<Product> products = productDAO.sortProducts("quantity", "Desc");
+                request.setAttribute("products", products);
+                disp.forward(request, response);
+                service = "Desc";
+                }
+            }
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/dao/ProductDAO.java
+++ src/java/dao/ProductDAO.java
@@ -1,7 +1,9 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -59,5 +61,61 @@ public class ProductDAO extends jdbc.DBConnect {
             Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return listP;
+    }
+    public Vector<Product> searchProducts(String statement, String sortOrder) {
+        String sql = statement + sortOrder;
+        Vector<Product> searchlistProduct = new Vector<>();
+        try {
+            ResultSet rs = getData(sql);
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                int categoryId = rs.getInt(2);
+                int providerId = rs.getInt(3);
+                String name = rs.getString(4);
+                String description = rs.getString(5);
+                float price = rs.getFloat(6);
+                float discount = rs.getFloat(7);
+                int quantity = rs.getInt(8);
+                String image = rs.getString(9);
+                searchlistProduct.add(new Product(id, categoryId, providerId, name, description, price, discount, quantity, image));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return searchlistProduct;
+    }
+    
+    public Vector<Product> sortProducts(String sortField, String sortOrder) {
+        String sql = "SELECT * FROM product ORDER BY " + sortField + " " + sortOrder;
+        Vector<Product> sortlistProduct = new Vector<>();
+        try {
+            ResultSet rs = getData(sql);
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                int categoryId = rs.getInt(2);
+                int providerId = rs.getInt(3);
+                String name = rs.getString(4);
+                String description = rs.getString(5);
+                float price = rs.getFloat(6);
+                float discount = rs.getFloat(7);
+                int quantity = rs.getInt(8);
+                String image = rs.getString(9);
+                sortlistProduct.add(new Product(id, categoryId, providerId, name, description, price, discount, quantity, image));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sortlistProduct;
+    }
+
+    public int updateQty(int id, int qty) throws SQLException {
+        String sql = "UPDATE Product\n"
+                + "SET quantity = ? \n"
+                + "WHERE id = ?";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setInt(1, qty);
+        statement.setInt(2, id);
+        int n = statement.executeUpdate();
+        return n;
     }
 }
